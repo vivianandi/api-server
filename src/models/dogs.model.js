bb906a31b156817eaed27322167c091b0ce61f0e
Vivian Davila <viviananddav@gmail.com>
2024-05-02 13:15:50 -0700

    Edit dog model

--- src/models/dogs.model.js
+++ src/models/dogs.model.js
@@ -1,22 +1,17 @@
 'use strict';
 
-// const { DataTypes } = require('sequelize');
-// const { dbConnect } = require('./index.model');
-
-const Dogs = (dbConnect, DataTypes) => {
-  return dbConnect.define('Dogs', {
-    dogName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    dogBreed: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    dogMood: {
-      type: DataTypes.STRING
-    },
-  });
-};
+const Dogs = (sequelize, DataTypes) => sequelize.define('Dogs', {
+  dogName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  dogBreed: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  dogMood: {
+    type: DataTypes.STRING
+  },
+});
 
 module.exports = Dogs;
